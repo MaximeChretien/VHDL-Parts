d4be14d3f6eaf8373306030e74921a1fe3855784
MixLeNain <mchretien@linuxmail.org>
2019-05-03 14:17:08 +0200

    PWM: make it more generic and comment code

--- PWM.vhd
+++ PWM.vhd
@@ -3,32 +3,35 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity PWM is 
+	generic(
+		constant F_clk	:	integer		:=	50000000; --input clock frequency --use ~5000 for sim
+		constant F_PWM	:	integer		:=	500		  --PWM frequency
+	);
 	port(
-		clk_50MHz 	:	in std_logic;
-		duty			:	in	integer range 0 to 255;
-		output	 	:	out std_logic
+		clk 		:	in std_logic;	-- input clock
+		duty		:	in	integer range 0 to 255;	--duty cycle value
+		PWMout	 	:	out std_logic  -- Output signal
 	);
 end PWM;
 
 architecture PWM of PWM is
-	signal	DutyCount		:	integer range 0 to 255 		:= 0;
-	
-	constant F_50MHz			:	integer							:=	50000000; --use 5000 for sim
-	constant F_PWM				:	integer							:=	500;
-	constant	Div_PWM			:	integer 							:= F_50MHz / F_PWM / 2;
-	signal	CountPWM_clk	:	integer range 0 to Div_PWM := 0;
-	signal	PWM_clk			:	std_logic 						:= '0';
+	signal	 DutyCount	:	integer range 0 to 255 		:= 0;	-- Counter for duty cycle
+	constant Div_PWM	:	integer 					:= F_clk / F_PWM / 2; -- frequency divider value
+	signal	 PWMCount	:	integer range 0 to Div_PWM 	:= 0;		-- frequency divider counter
+	signal	 PWM_clk	:	std_logic 					:= '0';		-- PWM clock
 begin
+	-- frequency divider
 	process begin
-		wait until rising_edge(clk_50MHz);
-		if(CountPWM_clk = 0) then
-			CountPWM_clk <= Div_PWM - 1;
+		wait until rising_edge(clk);
+		if(PWMCount = 0) then
+			PWMCount <= Div_PWM - 1;
 			PWM_clk <= not PWM_clk;
 		else
-			CountPWM_clk <= CountPWM_clk - 1;
+			PWMCount <= PWMCount - 1;
 		end if;
 	end process;
 	
+	-- Duty cycle
 	process begin
 		wait until rising_edge(PWM_clk);
 		if(DutyCount = 255) then
@@ -38,5 +41,6 @@ begin
 		end if;
 	end process;
 	
-	output <= '1' when DutyCount < duty else '0';
+	-- output
+	PWMout <= '1' when DutyCount < duty else '0';
 end PWM;
