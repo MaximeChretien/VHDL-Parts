ba9fdc53b255e1362fd837d44429aa14fe3fef22
MixLeNain <mchretien@linuxmail.org>
2019-05-09 14:16:22 +0200

    vhdl_parts: indent with spaces

--- vhdl_parts.vhd
+++ vhdl_parts.vhd
@@ -9,19 +9,19 @@ use ieee.std_logic_1164.all;
 
 package vhdl_parts is
 
-	-- PWM Generator
-	component PWM is
-		generic(
-			constant F_clk	    :	integer	:=	5000;   --input clock frequency
-			constant F_PWM	    :	integer	:=	500;	--PWM frequency
-			constant Precision  :   integer	:=	4		--Duty Cycle precision (bits number)
-		);
-		port(
-			clk 	:	in  std_logic;	-- input clock
-			duty	:	in  std_logic_vector(Precision-1 downto 0);	--duty cycle value
-			PWMout	:	out std_logic  -- Output signal
-		);
-	end component;
+    -- PWM Generator
+    component PWM is
+        generic(
+            constant F_clk     : integer := 5000; --input clock frequency
+            constant F_PWM     : integer := 500;  --PWM frequency
+            constant Precision : integer := 4     --Duty Cycle precision (bits number)
+        );
+        port(
+            clk    : in  std_logic;	-- input clock
+            duty   : in  std_logic_vector(Precision-1 downto 0); --duty cycle value
+            PWMout : out std_logic -- Output signal
+        );
+    end component;
 
 
 end vhdl_parts;
